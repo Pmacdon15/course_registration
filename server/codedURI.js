7cc60db6faac968e0af8a8801ec0f8619dd40243
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2023-11-19 13:09:07 -0700

    upgraded codedURI.js

--- server/codedURI.js
+++ server/codedURI.js
@@ -1,4 +1,5 @@
 const readline = require('readline');
+const copyPaste = require('copy-paste');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -8,6 +9,10 @@ const rl = readline.createInterface({
 rl.question("Enter the words to change: ", (wordsToChange) => {
   const codedUri = encodeURIComponent(wordsToChange);
   console.log(codedUri);
+
+  // Copy the encoded URI to the clipboard
+  copyPaste.copy(codedUri);
+  console.log('Encoded URI copied to clipboard.');
 
   rl.close();
 });
